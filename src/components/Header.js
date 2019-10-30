8db21d9e1d9cc21a7f82ec70ea13f6ed5adf204a
Simon Phillips <simon.phillips01@icloud.com>
2019-10-30 12:47:33 +0000

    Creating and toggling login / register forms

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter, NavLink } from "react-router-dom";
 
 function Header() {
   return (
@@ -13,25 +13,25 @@ function Header() {
           new
         </NavLink>
         <div className="divider">|</div>
-        <NavLink to="top" className="header-link">
+        <NavLink to="/top" className="header-link">
           top
         </NavLink>
         <div className="divider">|</div>
-        <NavLink to="search" className="header-link">
+        <NavLink to="/search" className="header-link">
           search
         </NavLink>
         <div className="divider">|</div>
-        <NavLink to="create" className="header-link">
+        <NavLink to="/create" className="header-link">
           submit
         </NavLink>
       </div>
       <div className="flex">
-        <NavLink to="login" className="header-link">
+        <NavLink to="/login" className="header-link">
           login
         </NavLink>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header;
+export default withRouter(Header);
