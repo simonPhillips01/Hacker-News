662cf3f44fcd26b3102e512bc3e70ed390c218a0
Simon Phillips <simon.phillips01@icloud.com>
2019-11-01 17:18:54 +0000

    Adding firestore and creating links

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 import firebaseConfig from './config';
 
@@ -9,6 +10,7 @@ class Firebase {
         // firebase.initializeApp(firebaseConfig);
         app.initializeApp(firebaseConfig);
         this.auth = app.auth()
+        this.db = app.firestore();
     }
 
     async register(name, email, password) {
